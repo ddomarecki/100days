351c4bd83baa84159b6662efac59f9bf9c94e0dd
ddomarecki <damian.domarecki@gmail.com>
2025-04-26 15:04:08 +0200

    second lesson

--- 1/lesson2.py
+++ 1/lesson2.py
@@ -1,1 +1,10 @@
-print(len("1234456"))
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input ("What te percentage tip woluld like to give 10, 12, 15?"))
+people = int(input("Howa many peo9ple to split the bill?"))
+
+bill_with_tip = tip/ 100 * bill + bill
+bill_per_person = bill_with_tip / people
+final_amount = round(bill_per_person, 2)
+
+print(f"Each person should pay {final_amount}")

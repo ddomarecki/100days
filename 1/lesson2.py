8c40128bd8d4351350b6bb78dfb1d08db0d520cc
ddomarecki <damian.domarecki@gmail.com>
2025-04-27 22:28:32 +0200

    3lesson

--- 1/lesson2.py
+++ 1/lesson2.py
@@ -1,10 +1,10 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input ("What te percentage tip woluld like to give 10, 12, 15?"))
-people = int(input("Howa many peo9ple to split the bill?"))
+# lesson 3
 
-bill_with_tip = tip/ 100 * bill + bill
-bill_per_person = bill_with_tip / people
-final_amount = round(bill_per_person, 2)
+print("welcome")
+height = int(input("What is ur height in cm?"))
 
-print(f"Each person should pay {final_amount}")
+if height > 120:
+    print("u can ride")
+else:
+    print("u can not ride")
+

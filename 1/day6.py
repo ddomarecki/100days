d94ecb1bdd90218d3c7e5cec13cc1fd584969a71
ddomarecki <damian.domarecki@gmail.com>
2025-05-08 22:46:56 +0200

    hurdle3

---
+++ 1/day6.py
@@ -0,0 +1,24 @@
+#Hurdle 3 from Reeborg's World
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        turn_left()
+        move()
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+        turn_left()
+        turn_left()
+        turn_left()
+        turn_left()
+
+    else:
+        move()

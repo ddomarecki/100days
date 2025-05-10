ca431cd9c3f738343fae08765ea2ceb6a69a254b
ddomarecki <damian.domarecki@gmail.com>
2025-05-10 16:46:02 +0200

    hurdle 4

--- 1/day6.py
+++ 1/day6.py
@@ -5,16 +5,47 @@ def turn_right():
     turn_left()
     turn_left()
 
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
 
 while not at_goal():
     if wall_in_front():
-        turn_left()
+        jump()
+
+    else:
         move()
-        turn_right()
+
+#Hurdle 4
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
         move()
-        turn_right()
+    turn_right()
+    move()
+    turn_right()
+
+    while front_is_clear():
         move()
-        turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
 
     else:
         move()

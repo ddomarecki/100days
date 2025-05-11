42349daaea78ef1fd12a32e15af0a617593fbb02
ddomarecki <damian.domarecki@gmail.com>
2025-05-11 14:20:20 +0200

    Day 8

---
+++ 1/day8.py
@@ -0,0 +1,38 @@
+
+# def greet():
+#     print("Hi")
+#     print("Hi")
+#
+# greet()
+
+# inputs
+
+# def greet_with_name(name):
+#     print(f"hello {name}")
+# greet_with_name("Damian")
+
+# functions with more than 1 input
+
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"How is it like in {location}")
+#
+# greet_with(name = "Damian", location = "Poznań  ")
+
+# def calculate_love_score(name1, name2):
+#     combined_names = (name1 + name2).lower()
+#
+#     true_count = 0
+#     for letter in "true":
+#         true_count += combined_names.count(letter)
+#
+#     love_count = 0
+#     for letter in "love":
+#         love_count += combined_names.count(letter)
+#
+#     love_score = int(str(true_count) + str(love_count))
+#     print(love_score)
+#
+# # Przykładowe wywołanie:
+# calculate_love_score("Anna Graczyk", "Damian Domarecki")
+

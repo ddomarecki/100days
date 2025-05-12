406589fcc09213df0b96a6d09c554ff3a1372f54
ddomarecki <damian.domarecki@gmail.com>
2025-05-12 18:37:17 +0200

    Day 10 excersise

--- 1/day10.py
+++ 1/day10.py
@@ -11,3 +11,12 @@ def format_name(f_name, l_name):
 formated_string = format_name("damian", "DomareckI")
 print(formated_string)
 
+# def is_leap_year(year):
+#     if year % 4 != 0:
+#         return False
+#     elif year % 100 != 0:
+#         return True
+#     elif year % 400 == 0:
+#         return True
+#     else:
+#         return False

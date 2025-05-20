794188be0e6065e26842ccdd44181108eb2776d1
ddomarecki <damian.domarecki@gmail.com>
2025-05-20 20:41:57 +0200

    day 16 - OOP v. 1

---
+++ 1/day16.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("chartreuse4")
+timmy.forward(100)
+
+my_screen = Screen()
+
+my_screen.canvheight
+
+my_screen.exitonclick()
+
+

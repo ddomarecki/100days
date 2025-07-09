8b28d1c308e5d08de0adf414f9687e6134bb28a0
ddomarecki <damian.domarecki@gmail.com>
2025-07-09 22:45:10 +0200

    day 27 part 2

--- 1/gui-tkinter/main.py
+++ 1/gui-tkinter/main.py
@@ -1,14 +1,36 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 #label
 
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+#Button
+
+def button_clicked():
+    new_text = input.get()
+    print("I got clicked")
+    my_label.config(text=new_text)
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+#Entry
+
+input = Entry(width=20)
+input.pack()
+input.get()
+
+
+
 
 def add(*args):
     sum = 0
@@ -17,6 +39,17 @@ def add(*args):
     return sum
 
 print(add(3, 5, 6, 2, 1))
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+calculate(2, add=3, multiply=5)
+
 
 
 window.mainloop()
